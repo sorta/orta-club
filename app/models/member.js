ecb4ceff928683f164352306aef544cf6280b595
Sophie Orta <sophie@travis-ci.org>
2020-09-26 10:16:37 -0400

    Fix birthdate setting

--- app/models/member.js
+++ app/models/member.js
@@ -9,4 +9,25 @@ export default class MemberModel extends Model {
   @attr('boolean') isApproved;
 
   @belongsTo('user') user;
+
+  get formattedBirthdate() {
+    const { birthdate } = this;
+    if (!birthdate) {
+      return null;
+    }
+
+    const bd = new Date(birthdate);
+
+    let month = bd.getMonth() + 1;
+    if (month < 10) {
+      month = `0${month}`;
+    }
+
+    let day = bd.getDate();
+    if (day < 10) {
+      day = `0${day}`;
+    }
+
+    return `${bd.getFullYear()}-${month}-${day}`;
+  }
 }
