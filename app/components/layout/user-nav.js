693cbe97c7fa7427915cb9fd040e577cd4bbb03d
Sophie Orta <sophie@travis-ci.org>
2020-09-27 11:38:29 -0400

    Fix logout

--- app/components/layout/user-nav.js
+++ app/components/layout/user-nav.js
@@ -3,6 +3,7 @@ import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
 export default class LayoutUserNavComponent extends Component {
+  @service auth;
   @service userNav;
 
   @action
