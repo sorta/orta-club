8b385f97ef24e641567131bfb1baa40240d7276b
Sophie Orta <sophie@travis-ci.org>
2020-10-02 10:47:05 -0400

    Fix tests

--- tests/integration/components/layout/user-nav-test.js
+++ tests/integration/components/layout/user-nav-test.js
@@ -3,24 +3,15 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | layout/user-nav', function(hooks) {
+module('Integration | Component | layout/user-nav', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+  test('it renders', async function (assert) {
     await render(hbs`<Layout::UserNav />`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <Layout::UserNav>
-        template block text
-      </Layout::UserNav>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.ok(
+      this.element.textContent.includes('My Account'),
+      'Include My Account',
+    );
   });
 });
