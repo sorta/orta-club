2577192cf096b007f61a0f90d8cffdcdb7acdfaf
sorta <orta.sophie@gmail.com>
2022-10-02 10:03:35 -0400

    Update to 4.0

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,12 @@
 import Application from 'orta-club/app';
 import config from 'orta-club/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
